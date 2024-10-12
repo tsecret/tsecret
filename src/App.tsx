10b2b0492144ed301e5329764a5a17b0629e6f06
tsecret <timichfull@gmail.com>
2024-10-13 00:21:24 +0500

    fixed align

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export const App = () => {
     <section id="home" className='row space-x-4'>
       <p className='text-[12rem]'>👨🏻‍💻</p>
 
-      <div className='col text-3xl'>
+      <div className='col text-3xl text-center'>
         <p>I'm Timur</p>
         <p className='text-primary'>Software Engineer</p>
       </div>
